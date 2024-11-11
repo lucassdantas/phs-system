c72a05005e1f58b53c8a877dfb1a3149a86ac611
Lucas Dantas <lucasdantasprogramador@gmail.com>
2024-11-11 18:52:10 -0300

    finish tabs design

--- src/pages/MyAccount/index.tsx
+++ src/pages/MyAccount/index.tsx
@@ -10,6 +10,7 @@ import { BiUser } from "react-icons/bi";
 import { TbUserSquare } from "react-icons/tb";
 import { IoBag } from "react-icons/io5";
 import { FaBookReader } from "react-icons/fa";
+import { IoMdExit } from "react-icons/io";
 
 interface Tabs{
   icon:ReactNode;
@@ -23,22 +24,28 @@ export const MyAccount = () => {
   const handleTabClick = (tab: string) => {
     setActiveTab(tab);
   };
+
   const tabsAndIcons:Tabs[] = [
     {icon:<FaBookReader/>, content:'Meus cursos'},
     {icon:<BiUser/>, content:'Meu perfil'}, 
     {icon:<TbUserSquare/>, content:'Endereço'}, 
     {icon:<IoBag/>, content:'Meus pedidos'}
   ]
+
   return (
     <Template pageTitle='Minha conta'>
       <Section>
-        <button onClick={handleLogout}>Logout</button>
-        <div className="flex border-b gap-4 border-gray-200 w-full justify-between">
+        <div className="flex lg:flex-row flex-col gap-4 border-gray-200 w-full justify-between pt-12 pb-6">
           {tabsAndIcons.map((tab:Tabs) => (
-            <button key={tab.content} onClick={() => handleTabClick(tab.content)} className={`flex w-1/4 justify-center gap-4 items-center p-4 text-lg font-bold text-white rounded-full hover:bg-medium-green-phs-system ${activeTab === tab.content ? 'bg-medium-green-phs-system' : 'bg-light-blue-phs-system'} cursor-pointer transition duration-300 hover:scale-110`}>
+            <button key={tab.content} onClick={() => handleTabClick(tab.content)} className={`flex w-1/4 justify-center gap-4 items-center p-4 text-lg font-bold text-white rounded-full hover:bg-medium-green-phs-system ${activeTab === tab.content ? 'bg-medium-green-phs-system' : 'bg-medium-blue-phs-system'} cursor-pointer transition duration-300 hover:scale-110`}>
               {tab.icon} {tab.content}
             </button>
           ))}
+        </div>
+        <div className="w-full flex justify-end">
+          <button onClick={handleLogout} className={`flex justify-center gap-4 items-center py-2 px-6 text-lg  text-white rounded-full bg-medium-blue-phs-system hover:bg-red-700 cursor-pointer transition duration-300 hover:scale-110`}>
+            <IoMdExit fontWeight={20} className='font-bold'/> Sair
+          </button>
         </div>
 
         <div className="p-4">
