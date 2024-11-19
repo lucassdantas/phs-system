b776c6345c3f88ff824bd2ae750b814060634068
Lucas Dantas <lucasdantasprogramador@gmail.com>
2024-11-19 01:56:59 -0300

    building dropwdown men to add items

--- src/components/Header/DropdownMenu.tsx
+++ src/components/Header/DropdownMenu.tsx
@@ -1,14 +1,20 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FaChevronDown } from 'react-icons/fa'; // Importando o ícone de setinha
 
 interface DropdrownMenuProps{
-  dropdownItems:{
-    route:string;
-    content:string;
-  }[]
+  dropdownMenu:{
+    mainMenu:{
+      route:string;
+      content:string;
+    };
+    dropdownItems:{
+      route:string;
+      content:string;
+    }[] 
+  };
 }
-const DropdownMenu = ({dropdownItems}:DropdrownMenuProps) => {
+const DropdownMenu = ({dropdownMenu}:DropdrownMenuProps) => {
   const [isOpen, setIsOpen] = useState(false);
 
   const handleMouseEnter = () => {
@@ -21,10 +27,10 @@ const DropdownMenu = ({dropdownItems}:DropdrownMenuProps) => {
 
   return (
     <div className="relative" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} >
-      <span className="text-gray-700 hover:text-light-blue-phs-system flex items-center" > <Link to='/aulas'>Aulas</Link> <FaChevronDown className="ml-1 text-xs" /> </span>
+      <span className="text-gray-700 hover:text-light-blue-phs-system flex items-center" > <Link to={dropdownMenu.mainMenu.route}>{dropdownMenu.mainMenu.content}</Link> <FaChevronDown className="ml-1 text-xs" /> </span>
       {isOpen && (
         <div className="absolute z-20 py-2 w-48 bg-white shadow-lg rounded-md">
-          {dropdownItems.map((item, i) => (
+          {dropdownMenu.dropdownItems.map((item, i) => (
             <Link key={i} to={item.route} className="block px-4 py-2 text-gray-700 hover:bg-gray-200">{item.content}</Link>
           ))}
         </div>
