6e20a7d9745533aaf4affcc2a079783c3faf2031
Lucas Dantas <lucasdantasprogramador@gmail.com>
2024-11-19 17:38:42 -0300

    idk

--- src/pages/Admin/AddEntity/AddLesson/index.tsx
+++ src/pages/Admin/AddEntity/AddLesson/index.tsx
@@ -1,6 +1,7 @@
 import { ColoredContainer } from '@/components/ColoredContainer'
 import { Section } from '@/components/Section'
 import { Template } from '@/components/Template'
+import { userImagesDirectory } from '@/utils/constants/siteInfos'
 import React from 'react'
 
 export const AddLesson = () => {
@@ -16,18 +17,30 @@ export const AddLesson = () => {
       <Section className='py-12'>
         <div className="flex gap-12">
           <div className='w-2/6'>
-            <nav className='border border-neutral-400 rounded-xl p-4 pl-0 space-y-4 sticky'>
-              {navMenu.map((item, index) => (
-                <li key={index} className='border-medium-blue-phs-system border-l-2 list-none pl-4'>
-                  <a href={item.url}>{item.content}</a>
-                </li>
-              ))}
+            <nav className='lg:pr-16'>
+              <ul className='border border-neutral-300 rounded-xl py-12 pl-0 space-y-4 sticky'>
+                {navMenu.map((item, index) => (
+                  <li key={index} className='border-medium-blue-phs-system hover:border-medium-green-phs-system border-l-2 list-none pl-4'>
+                    <a href={item.url}>{item.content}</a>
+                  </li>
+                ))}
+              </ul>
             </nav>
           </div>
           <div className='w-5/6 space-y-5'>
             <h2 className='text-3xl font-bold'>Informações do curso</h2>
-            <ColoredContainer cardTitle='Instrutor'>
-              a
+            <ColoredContainer>
+              <div className="flex gap-12">
+                <div className='w-2/6'>
+                  <img src={userImagesDirectory+'default-image/placeholder-image.jpg'} alt='Foto de perfil do usuário' className='rounded-full w-full h-auto object-cover aspect-square' />
+                </div>
+                <div className='w-5/6 space-y-5'>
+                  <ColoredContainer>
+                    <h3>Nome do instrutor</h3>
+                    <p>Descrição do instrutor</p>
+                  </ColoredContainer>
+                </div>
+              </div>
             </ColoredContainer>
 
             <ColoredContainer cardTitle='Nome e descrição do curso'>
