fb6ed8f03e3c78c6fa0336798ef8fa5264e10ff1
Lucas Dantas <lucasdantasprogramador@gmail.com>
2024-11-19 23:57:06 -0300

    condition to header

--- src/components/Header/NavMenu.tsx
+++ src/components/Header/NavMenu.tsx
@@ -1,11 +1,13 @@
 import { Link } from 'react-router-dom';
 import { adminDropdownMenu, lessonsDropdownMenu } from '@/components/Header/utils/menus';
 import DropdownMenu from './DropdownMenu';
+import { useAuth } from '@/hooks/UseAuth';
 
 const NavMenu = () => {
+  const { user } = useAuth();
   return (
     <nav className="lg:flex lg:flex-row lg:space-y-0 lg:py-0 lg:space-x-8 flex-col items-start py-4">
-      <DropdownMenu dropdownMenu={adminDropdownMenu}/> 
+      { user.user_role==='admin' && <DropdownMenu dropdownMenu={adminDropdownMenu}/> }
       <DropdownMenu dropdownMenu={lessonsDropdownMenu}/> 
       <Link to="/minha-conta" className="text-gray-700 hover:text-light-blue-phs-system">Minha conta</Link>
     </nav>
